50c851739d9d25ddac346f8b7185249d307aacc8
internetisgone <schumann777@proton.me>
2023-05-28 16:27:18 +0800

    run one task in each channel

--- main.py
+++ main.py
@@ -6,37 +6,58 @@ import re
 import asyncio
 from dotenv import load_dotenv
 
-PROXY = "http://127.0.0.1:1087" 
-DEFAULT_DURATION = "2h"
-PURGE_INTERVAL = 11 # in seconds
-running = False
-# PROXY = None
-# SERVER_WHITELIST = []
-
 load_dotenv()
+
+COMMAND_PREFIX = "!"
+DEFAULT_DURATION = "2h"
+PURGE_INTERVAL = 33 # in seconds
+
+# for dev #
+PROXY = "http://127.0.0.1:1087" 
+DISCORD_KEY = os.getenv("DISCORD_KEY_TEST")
+
+# for production #
+# PROXY = None
+# DISCORD_KEY = os.getenv("DISCORD_KEY")
+
+active_tasks = {} # key: channel id, value: task
 
 def run_bot():
     intents = discord.Intents.default()
     intents.messages = True
     intents.message_content = True
-    bot = None
-    if PROXY != None:
-        bot = commands.Bot(intents = intents, command_prefix='!', proxy = PROXY)  
-    else:
-        bot = commands.Bot(intents = intents, command_prefix='!')
-    
+
+    bot = commands.Bot(intents = intents, command_prefix = COMMAND_PREFIX, proxy = PROXY)  
+
     @bot.event
     async def on_ready():
         print(f"{bot.user} is running")
 
-    @bot.command(name="kms") 
+    # @tasks.loop(seconds = PURGE_INTERVAL, reconnect = True)
+    async def purge_channel(channel, dtime, self_msg_id):
+        print(f"kms every {dtime} in channel {channel}")
+        await channel.purge(
+            limit = None, 
+            check = lambda msg: not msg.pinned and not msg.id == self_msg_id, 
+            before = datetime.now() - dtime,
+            oldest_first = True
+        )   
+
+    async def start_purge_task_loop(channel, dtime, self_msg_id):
+        # restart task if already running in a certain channel
+        if channel.id in active_tasks:
+            print(f"restarting task {active_tasks[channel.id]} in channel {channel}")
+            active_tasks[channel.id].stop()
+
+        interval = dtime.total_seconds() if dtime.total_seconds() < PURGE_INTERVAL else PURGE_INTERVAL
+
+        new_task = tasks.loop(seconds = interval, reconnect = True)(purge_channel)
+        active_tasks[channel.id] = new_task
+        new_task.start(channel, dtime, self_msg_id)
+
+    @bot.command(name = "kms") 
     async def set_duration(ctx, duration):
         try:
-            # global running
-            # if running:
-            #     purge_channel.stop()
-            #     running = False
-
             # parse duration 
             duration = re.search('\d+[smh]', duration)
             dtime = None
@@ -55,40 +76,17 @@ def run_bot():
                 dtime = timedelta(hours = int(num.group(0)))
 
             self_msg = await ctx.channel.send(f"messages in this channel will be deleted after {duration}")
-            print(dtime)
+            print(dtime) 
 
-            @tasks.loop(seconds = PURGE_INTERVAL, reconnect = True)
-            async def purge_channel():
-                await ctx.channel.purge(
-                    limit = None, 
-                    check = lambda msg: not msg.pinned and not msg.id == self_msg.id, 
-                    before = datetime.now() - dtime,
-                    oldest_first = True
-                )    
-                # global running 
-                # running = True     
-                # await ctx.channel.send("earth server has been wiped :)")
+            # start task in a certain channel
+            await start_purge_task_loop(ctx.channel, dtime, self_msg.id)
 
-            # @tasks.loop(seconds = dtime.total_seconds(), reconnect = True)
-            # async for msg in ctx.channel.history():
-            #     time_diff = (discord.utils.utcnow() - msg.created_at).total_seconds()
-            #     # ignore pinned msg and msg sent by the bot itself
-            #     if time_diff > dtime.total_seconds() and not msg.pinned and not msg.author == bot.user: 
-            #         await msg.delete()
-            #         # await ctx.channel.send(f"deleting msg {msg}")
-            #         await asyncio.sleep(1) # avoid rate limits
-            
-
-            # start the task
-            purge_channel.start()
-
-            
         except Exception as e:
             print(e)
             await ctx.channel.send(f"kms failed, {e}")
 
 
-    bot.run(os.getenv("DISCORD_KEY"))
+    bot.run(DISCORD_KEY)
 
 if __name__ == '__main__':
     run_bot()
