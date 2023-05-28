68ef96729480a4876d8bd484948dd9caf501c234
internetisgone <schumann777@proton.me>
2023-05-28 17:11:55 +0800

    add readme, cleanup

---
+++ README.md
@@ -0,0 +1,21 @@
+# autodelete-discord-bot
+
+deletes messages in a discord channel after a certain amount of time. supports different settings in multiple channels. pinned messages are kept.
+
+## usage
+`!kms 24h`<br>
+`!kms 30m`<br>
+`!kms 1s`<br>
+
+## running your own instance
+- install the requirements with `pip3 install -r requirements.txt`
+- create a `.env` file using the template below
+- go to discord developer portal and create a new application 
+- in the "bot" section, copy the bot's token and paste it in `.env`. also enable the `message content intent` in `privileged gateway intents`. optional: uncheck `public bot`
+- in the "OAuth2 - URL generator" section, set scope to `bot`, and select the `send messages`, `manage messages`, and `read message history` permissions. generate an invite link 
+- invite the bot to your server with the link and run main.py
+
+## .env
+```
+DISCORD_KEY = "123456"
+```
--- main.py
+++ main.py
@@ -3,14 +3,9 @@ import discord
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta
 import re
-import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
-
-COMMAND_PREFIX = "!"
-DEFAULT_DURATION = "2h"
-PURGE_INTERVAL = 33 # in seconds
 
 # for dev #
 PROXY = "http://127.0.0.1:1087" 
@@ -20,7 +15,32 @@ DISCORD_KEY = os.getenv("DISCORD_KEY_TEST")
 # PROXY = None
 # DISCORD_KEY = os.getenv("DISCORD_KEY")
 
+COMMAND_PREFIX = "!"
+DEFAULT_DURATION = "2h"
+PURGE_INTERVAL = 33 # in seconds
+
 active_tasks = {} # key: channel id, value: task
+
+async def purge_channel(channel, dtime, self_msg_id):
+    # print(f"kms every {dtime} in channel {channel}")
+    await channel.purge(
+        limit = None, 
+        check = lambda msg: not msg.pinned and not msg.id == self_msg_id, 
+        before = datetime.now() - dtime,
+        oldest_first = True
+    )   
+
+async def start_purge_task_loop(channel, dtime, self_msg_id):
+    # restart task if already running this channel
+    if channel.id in active_tasks:
+        # print(f"restarting task {active_tasks[channel.id]} in channel {channel}")
+        active_tasks[channel.id].stop()
+
+    interval = dtime.total_seconds() if dtime.total_seconds() < PURGE_INTERVAL else PURGE_INTERVAL
+
+    new_task = tasks.loop(seconds = interval, reconnect = True)(purge_channel)
+    active_tasks[channel.id] = new_task
+    new_task.start(channel, dtime, self_msg_id)
 
 def run_bot():
     intents = discord.Intents.default()
@@ -33,28 +53,6 @@ def run_bot():
     async def on_ready():
         print(f"{bot.user} is running")
 
-    # @tasks.loop(seconds = PURGE_INTERVAL, reconnect = True)
-    async def purge_channel(channel, dtime, self_msg_id):
-        print(f"kms every {dtime} in channel {channel}")
-        await channel.purge(
-            limit = None, 
-            check = lambda msg: not msg.pinned and not msg.id == self_msg_id, 
-            before = datetime.now() - dtime,
-            oldest_first = True
-        )   
-
-    async def start_purge_task_loop(channel, dtime, self_msg_id):
-        # restart task if already running in a certain channel
-        if channel.id in active_tasks:
-            print(f"restarting task {active_tasks[channel.id]} in channel {channel}")
-            active_tasks[channel.id].stop()
-
-        interval = dtime.total_seconds() if dtime.total_seconds() < PURGE_INTERVAL else PURGE_INTERVAL
-
-        new_task = tasks.loop(seconds = interval, reconnect = True)(purge_channel)
-        active_tasks[channel.id] = new_task
-        new_task.start(channel, dtime, self_msg_id)
-
     @bot.command(name = "kms") 
     async def set_duration(ctx, duration):
         try:
@@ -62,7 +60,6 @@ def run_bot():
             duration = re.search('\d+[smh]', duration)
             dtime = None
             if not duration:
-                # await ctx.channel.send("duration should be a number followed by either `h`, `m`, or `s`")
                 duration = DEFAULT_DURATION
             else: 
                 duration = duration.group(0)
@@ -75,10 +72,10 @@ def run_bot():
             else: 
                 dtime = timedelta(hours = int(num.group(0)))
 
-            self_msg = await ctx.channel.send(f"messages in this channel will be deleted after {duration}")
+            self_msg = await ctx.channel.send(f"messages in this channel will be deleted after {duration}.")
             print(dtime) 
 
-            # start task in a certain channel
+            # start / restart task in a certain channel
             await start_purge_task_loop(ctx.channel, dtime, self_msg.id)
 
         except Exception as e:
